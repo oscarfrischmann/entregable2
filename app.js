32b8ffb236238f88fe1015d23d7ad16dd842bb07
Oscar Frischmann <oscarfrisc@gmail.com>
2024-04-06 14:38:45 -0300

    2da entrega CODERHOUSE backend

--- app.js
+++ app.js
@@ -1,14 +1,10 @@
 import fs from 'fs';
-const jsonProducts = [];
-const Products = [];
 
-// fs.existsSync('products.json')
-// 	? console.log('existe')
-// 	: fs.writeFileSync('./products.json', 'utf-8');
+const Products = [];
+fs.writeFileSync('./products.json', JSON.stringify(Products), 'utf-8');
 
 class ProductManager {
-	constructor(title, description, price, thumbnail, code, stock) {
-		// this.products = Products;
+	constructor(title, description, price, thumbnail, code, stock, path) {
 		this.title = title;
 		this.description = description;
 		this.price = price;
@@ -16,7 +12,7 @@ class ProductManager {
 		this.code = code;
 		this.stock = stock;
 		this.id;
-		this.path = './products.json';
+		this.path = path;
 	}
 
 	addProduct(product) {
@@ -32,16 +28,14 @@ class ProductManager {
 			if (existsInProducts) {
 				console.log(`The product with this code already exists.\n`);
 			} else {
-				Products.push(product);
-				console.log(`Producto ${this.code} agregado\n\n`);
-				console.log(Products);
 				(async () => {
 					try {
-						let productJSON = await JSON.stringify(product);
-						Products.push(productJSON);
-						await fs.appendFileSync('./products.json', productJSON, 'utf-8');
-					} catch (err) {
-						console.log(err);
+						await Products.push(product);
+						console.log(`Producto ${this.code} agregado\n\n`);
+						let productJSON = await JSON.stringify(Products);
+						await fs.promises.writeFile(this.path, productJSON, 'utf-8');
+					} catch (e) {
+						console.log('addProducts', e);
 					}
 				})();
 			}
@@ -49,64 +43,76 @@ class ProductManager {
 	}
 
 	getProducts() {
-		console.log('Productos agregados:');
-		Products.forEach((product) => {
-			console.log(`Title: ${product.title}`);
-			console.log(`Description: ${product.description}`);
-			console.log(`Price: $${product.price}`);
-			console.log(`Code: ${product.code}`);
-			console.log(`Code: ${product.id}`);
-			console.log(`Stock:  ${product.stock}\n\n`);
-		});
+		(async () => {
+			try {
+				let getProducts = await fs.promises.readFile('./products.json');
+				let parseProducts = await JSON.parse(getProducts);
+				await console.log('Productos agregados:');
+				await parseProducts.forEach((product) => {
+					console.log(`Title: ${product.title}`);
+					console.log(`Description: ${product.description}`);
+					console.log(`Price: $${product.price}`);
+					console.log(`Code: ${product.code}`);
+					console.log(`Code: ${product.id}`);
+					console.log(`Stock:  ${product.stock}\n\n`);
+				});
+			} catch (e) {
+				console.log('getProducts', e);
+			}
+		})();
 	}
 
 	getProductrById(id) {
-		console.log(`Producto filtrado por código: ${id}`);
+		(async () => {
+			let getProducts = await fs.promises.readFile('./products.json');
+			let parseProducts = await JSON.parse(getProducts);
 
-		let getCode = Products.find((product) => product.id === id);
-		if (getCode) {
-			for (let prop in getCode) {
-				if (prop === 'price') {
-					console.log(`${prop}: $${getCode[prop]}`);
-				} else {
-					console.log(`${prop}: ${getCode[prop]}`);
+			let getCode = await parseProducts.find((product) => product.id === id);
+			await console.log(`Producto filtrado por código: ${id}`);
+			if (getCode) {
+				for (let prop in getCode) {
+					if (prop === 'price') {
+						console.log(`${prop}: $${getCode[prop]}`);
+					} else {
+						console.log(`${prop}: ${getCode[prop]}`);
+					}
 				}
+			} else {
+				console.log(`Producto con id: ${id} NO ENCONTRADO`);
 			}
-		} else {
-			console.log('Product not found');
-		}
+		})();
 	}
 }
+
 let product;
 
-product = new ProductManager('Agua', 'Fresca', 10, 'http://google.com', 'AGU01', 5);
-product.products = 0;
+product = new ProductManager('Agua', 'Fresca', 'http://google.com', 'AGU01', 5, './products.json');
 product.addProduct(product);
 
-product = new ProductManager('Agua', 'Fresca', 10, 'http://google.com', 'AGU01', 5);
-product.products = 0;
+product = new ProductManager('Agua', 'Fresca', 10, 'http://google.com', 'AGU01', 5, './products.json');
 product.addProduct(product);
 
-product = new ProductManager('Vino', 'Mendocino', 15, 'http://yahoo.com', 'VIN01', 5);
-product.products = 0;
+product = new ProductManager('Agua', 'Fresca', 10, 'http://google.com', 'AGU01', 5, './products.json');
 product.addProduct(product);
 
-product = new ProductManager('cerveza', 'quilmes', 20, 'http://bing.com', 'CERV01', 5);
-product.products = 0;
+product = new ProductManager('Vino', 'Mendocino', 15, 'http://yahoo.com', 'VIN01', 5, './products.json');
 product.addProduct(product);
 
-product = new ProductManager('Agua', 'Fresca', 10, 'http://google.com', 'AGU01', 5);
-product.products = 0;
+product = new ProductManager('cerveza', 'quilmes', 20, 'http://bing.com', 'CERV01', 5, './products.json');
 product.addProduct(product);
 
-product = new ProductManager('Vino', 'Mendocino', 15, 'http://yahoo.com', 'VIN01', 5);
-product.products = 0;
+product = new ProductManager('Agua', 'Fresca', 10, 'http://google.com', 'AGU01', 5, './products.json');
 product.addProduct(product);
 
-product = new ProductManager('Gin', 'Inglés', 25, 'http://yahoo.com', 'GIN01', 5);
-product.products = 0;
+product = new ProductManager('Vino', 'Mendocino', 15, 'http://yahoo.com', 'VIN01', 5, './products.json');
+product.addProduct(product);
+
+product = new ProductManager('Gin', 'Inglés', 25, 'http://yahoo.com', 'GIN01', 5, './products.json');
+product.addProduct(product);
+
+product = new ProductManager('Coca-Cola', 'con hielo', 5, 'http://yahoo.com', 'KO01', 5, './products.json');
 product.addProduct(product);
 
 product.getProducts();
 
-product.getProductrById(1);
+product.getProductrById(3);
